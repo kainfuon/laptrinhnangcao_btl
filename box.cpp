435c175d4a736716b85ac7e517775a22df7bb386
kainfuon <100391627+kainfuon@users.noreply.github.com>
2022-05-22 14:14:07 +0700

    Update box.cpp

--- box.cpp
+++ box.cpp
@@ -20,7 +20,7 @@ void box (SDL_Renderer* renderer, int x, int y, int size)
 
 void drawmain (SDL_Renderer* renderer, int x, int y, int size)
 {
-    SDL_Texture* main = loadTexture("fish.png", renderer);
+    SDL_Texture* main = loadTexture("taungam6.jpg", renderer);
     SDL_Rect mainRect;
     SDL_QueryTexture(main, NULL, NULL, &mainRect.w, &mainRect.h);
     mainRect.x = x;
@@ -32,25 +32,37 @@ void drawmain (SDL_Renderer* renderer, int x, int y, int size)
     //main = NULL;
 }
 
-void threaten (SDL_Renderer* renderer, int x, int y, int size_obj)
+void threaten (SDL_Renderer* renderer, int x, int y, int size_obj_w, int size_obj_h)
 {
-    SDL_Texture* main = loadTexture("fish1.jpg", renderer);
+    SDL_Texture* main = loadTexture("threaten.jpg", renderer);
     SDL_Rect mainRect;
     SDL_QueryTexture(main, NULL, NULL, &mainRect.w, &mainRect.h);
     mainRect.x = x;
     mainRect.y = y;
-    mainRect.w = size_obj;
-    mainRect.h = size_obj;
+    mainRect.w = size_obj_w;
+    mainRect.h = size_obj_h;
     SDL_RenderCopy(renderer, main, NULL, &mainRect);
     SDL_DestroyTexture(main);
     //main = NULL;
 }
 
-
+void threaten1 (SDL_Renderer* renderer, int x, int y, int size_obj_w, int size_obj_h)
+{
+    SDL_Texture* main = loadTexture("threaten1.jpg", renderer);
+    SDL_Rect mainRect;
+    SDL_QueryTexture(main, NULL, NULL, &mainRect.w, &mainRect.h);
+    mainRect.x = x;
+    mainRect.y = y;
+    mainRect.w = size_obj_w;
+    mainRect.h = size_obj_h;
+    SDL_RenderCopy(renderer, main, NULL, &mainRect);
+    SDL_DestroyTexture(main);
+    //main = NULL;
+}
 
 void apple (SDL_Renderer* renderer, int x, int y, int size_obj)
 {
-    SDL_Texture* main = loadTexture("cherry.png", renderer);
+    SDL_Texture* main = loadTexture("flower.jpg", renderer);
     SDL_Rect mainRect;
     SDL_QueryTexture(main, NULL, NULL, &mainRect.w, &mainRect.h);
     mainRect.x = x;
@@ -64,13 +76,28 @@ void apple (SDL_Renderer* renderer, int x, int y, int size_obj)
 
 
 
-void drawbackground (SDL_Renderer* renderer, int x, int y)
+void drawbackground1 (SDL_Renderer* renderer, int x, int y)
 {
-    SDL_Texture* background = loadTexture("bg600.png", renderer);
+    SDL_Texture* background = loadTexture("bgr9.1.jpg", renderer);
     SDL_Rect backgroundRect;
     SDL_QueryTexture(background, NULL, NULL, &backgroundRect.w, &backgroundRect.h);
-    backgroundRect.x = 0;
-    backgroundRect.y = 0;
+    backgroundRect.x = x;
+    backgroundRect.y = y;
+    backgroundRect.w = 1200;
+    backgroundRect.h = 600;
+    SDL_RenderCopy(renderer, background, NULL, &backgroundRect);
+    SDL_DestroyTexture(background);
+}
+
+void drawbackground2 (SDL_Renderer* renderer, int x, int y)
+{
+    SDL_Texture* background = loadTexture("bgr9.2.jpg", renderer);
+    SDL_Rect backgroundRect;
+    SDL_QueryTexture(background, NULL, NULL, &backgroundRect.w, &backgroundRect.h);
+    backgroundRect.x = x;
+    backgroundRect.y = y;
+    backgroundRect.w = 1200;
+    backgroundRect.h = 600;
     SDL_RenderCopy(renderer, background, NULL, &backgroundRect);
     SDL_DestroyTexture(background);
 }
@@ -83,19 +110,26 @@ void move(int &x, int &y, int stepX, int stepY) {
 int random(int draw_y)
 {
     srand((int)time(0));
-    int r = 1 + rand() % 5;
+    int r = 0 + rand() % 5;
     draw_y = r*100;
     return draw_y;
 }
 
 bool inside(int &x, int &y, int stepX, int stepY, int SCREEN_WIDTH, int SCREEN_HEIGHT, int size) {
-        return (0 <= x && 0 <= y && x+stepX <= SCREEN_WIDTH - size + 5  && y+stepY <= SCREEN_HEIGHT - size +5 );
+        return (0 <= x && 0 <= y && x+stepX <= SCREEN_WIDTH - size -5  && y+stepY <= SCREEN_HEIGHT - size  );
 
 }
 
-bool check(int &x, int &y, int check_x, int check_y, int size, int size_obj)
+bool check(int &x, int &y, int check_x, int check_y, int size, int size_obj_w, int size_obj_h)
     {
-        if (x + size  >= check_x  && x <= check_x + size_obj &&
-            y >= check_y - size && y <= check_y + size_obj) return false;
+        if (x + size >= check_x + size_obj_w/2 && x <= check_x + size_obj_w/2 &&
+            y + size - size/5 >= check_y && y <= check_y + size_obj_h/2) return false;
         return true;
     }
+
+bool checkpoint (int &x, int &y, int check_x, int check_y, int size, int size_obj)
+{
+        if (x + size >= check_x + 5 && x <= check_x + size_obj - 5 &&
+            y + size - 5 >= check_y && y <= check_y + size_obj - 5) return false;
+        return true;
+}
